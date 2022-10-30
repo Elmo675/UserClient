102b132aa480a0508dfbcee74c014038ab763789
Emil Burdach <Emil.Burdach@gmail.com>
2022-10-30 17:45:43 +0100

    [UserClient-Added] UserClient added with users method implemented

---
+++ src/main/java/emil/burdach/userclient/service/UserClient.java
@@ -0,0 +1,23 @@
+package emil.burdach.userclient.service;
+
+import emil.burdach.userclient.model.dto.UserDTO;
+import emil.burdach.userclient.model.response.RandomUserResponse;
+import emil.burdach.userclient.service.client.RandomUserClient;
+import emil.burdach.userclient.service.mapper.RandomUserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserClient {
+
+    private final RandomUserClient randomUserClient;
+    private final RandomUserMapper randomUserMapper;
+
+    public List<UserDTO> users() {
+        RandomUserResponse randomUserResponse = randomUserClient.getRandomUsersResponse();
+        return randomUserMapper.mapToUserDTO(randomUserResponse.getResults());
+    }
+}
