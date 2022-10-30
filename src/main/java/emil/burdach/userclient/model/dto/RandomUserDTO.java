8d812f86259a9926ab1c2a4a58e480e5b6c82826
Emil Burdach <Emil.Burdach@gmail.com>
2022-10-30 19:04:04 +0100

    [UserClient-Added] added RandomUserClient and RandomUserDTO. Not implemented yet

---
+++ src/main/java/emil/burdach/userclient/model/dto/RandomUserDTO.java
@@ -0,0 +1,8 @@
+package emil.burdach.userclient.model.dto;
+
+import lombok.NoArgsConstructor;
+
+//TODO implement
+@NoArgsConstructor
+public class RandomUserDTO {
+}
---
+++ src/main/java/emil/burdach/userclient/service/client/RandomUserClient.java
@@ -0,0 +1,13 @@
+package emil.burdach.userclient.service.client;
+
+import emil.burdach.userclient.model.response.RandomUserResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RandomUserClient {
+
+    //TODO implement
+    public RandomUserResponse getRandomUsersResponse() {
+        return null;
+    }
+}
