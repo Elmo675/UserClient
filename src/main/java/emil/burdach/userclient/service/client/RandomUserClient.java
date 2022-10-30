342b5bfa0d38cde5d8cdb58027236b9d3d4a0b00
Emil Burdach <Emil.Burdach@gmail.com>
2022-10-30 22:00:40 +0100

    [RandomUserClient-Added] Implemented RandomUserClient with Test

--- src/main/java/emil/burdach/userclient/service/client/RandomUserClient.java
+++ src/main/java/emil/burdach/userclient/service/client/RandomUserClient.java
@@ -1,13 +1,46 @@
 package emil.burdach.userclient.service.client;
 
 import emil.burdach.userclient.model.response.RandomUserResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class RandomUserClient {
 
-    //TODO implement
+    private final RestTemplate restTemplate;
+
+    private final Logger log = LoggerFactory.getLogger(RandomUserClient.class);
+    private final static String URL = "https://randomuser.me/api/";
+
+    public RandomUserClient() {
+        restTemplate = new RestTemplateBuilder().build();
+    }
+
     public RandomUserResponse getRandomUsersResponse() {
-        return null;
+        log.info("Request to get RandomUsers");
+        try {
+            HttpHeaders headers = createHeaders();
+            HttpEntity<String> request = new HttpEntity<>(headers);
+            ResponseEntity<RandomUserResponse> response = restTemplate.exchange(URL, HttpMethod.GET, request, RandomUserResponse.class);
+            return response.getBody();
+        } catch (HttpServerErrorException | HttpClientErrorException e) {
+            log.error("Request failed. Exception status: {}, body: {}", e.getStatusCode(), e.getResponseBodyAsString());
+            throw e;
+        } catch (Exception e) {
+            log.error("Request failed. Exception Message: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    private HttpHeaders createHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
---
+++ src/test/java/emil/burdach/userclient/service/UserClientTest.java
@@ -0,0 +1,48 @@
+package emil.burdach.userclient.service;
+
+import emil.burdach.userclient.model.dto.UserDTO;
+import emil.burdach.userclient.service.client.RandomUserClient;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserClientTest {
+
+    UserClient userClient;
+
+    @BeforeAll
+    void setUp() {
+        userClient = new UserClient(new RandomUserClient());
+    }
+
+    @Test
+    void whenUsers_ThenTheyArePrintedProperlyAndListIsNotEmpty() {
+        //given
+        //when
+        List<UserDTO> users = userClient.users();
+        //then
+        assertThat(users.isEmpty()).isFalse();
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    void whenUsers_ThenNoFieldIsNullOrBlank() {
+        //given
+        //when
+        List<UserDTO> users = userClient.users();
+        //then
+        users.forEach(user->{
+            assertThat(user.getUuid()).isNotBlank();
+            assertThat(user.getFirstName()).isNotBlank();
+            assertThat(user.getLastName()).isNotBlank();
+            assertThat(user.getCity()).isNotBlank();
+            assertThat(user.getGender()).isNotNull();
+            assertThat(user.getRegistrationDate()).isNotNull();
+        });
+    }
+}
